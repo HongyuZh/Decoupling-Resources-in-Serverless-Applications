937ef732dfaa9e1bfdc63efd8de51a7f0b775f54
Hongyu <435984671@qq.com>
2022-09-17 18:18:08 +0800

    modified the experiment.py to solve the cold start problem and to output the heatmap about the data.

--- Preliminary Experiment/experiment.py
+++ Preliminary Experiment/experiment.py
@@ -1,7 +1,37 @@
 import os
+import time
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.use('TKAgg')
 
 
-def experiment(alloc, image_name, cnt, sudopassword):
+def configure(alloc):
+    container_name = []
+
+    # create a number of containers according to the allocation plan 
+
+    for i in range(0, len(alloc)):
+        try:
+            cmd = "docker run -d -t " + image_name + \
+                " -m " + str(alloc[j][0]) + " --cpus=" + str(alloc[j][1])
+            output = os.popen(cmd)
+            name = output.readlines()
+            container_name.append(name[0])
+
+        # if the execution fails, input "null" as the container's name
+
+        except:
+            print("couldn't start container in this condition \n")
+            container_name.append('null')
+            continue
+
+    return container_name
+
+
+def experiment(container_name, cnt, alloc):
     result = {}
 
     # repeat the experiment according to the given "cnt"
@@ -11,72 +41,85 @@ def experiment(alloc, image_name, cnt, sudopassword):
         for j in range(0, len(alloc)):
             experiment_result = result['experiment_'+str(i)]
 
-            # perform the commands below:
-            # let runtime equals -1 if a failure is detected
-            try:
-                # here I run the image and get the container name stored in "container_name"
-                cmd = "docker run -d -t " + image_name + \
-                    " -m " + str(alloc[j][0]) + " --cpus=" + str(alloc[j][1])
-                output = os.popen(cmd)
-                container_name = output.readlines()[0]
-                output.close()
-            except:
-                print("couldn't start container in this condition \n")
-                result['experiment'+str(i)][str(alloc[j])] = str(-1)
+            # let runtime equals -1 if a "null" is detected
+
+            if container_name[j] == "null":
+                experiment_result[str(alloc[j])] = -1
                 continue
 
-            # then record the runtime
-            # get the start time of the container
+            cmd = "docker restart " + container_name[j]
 
-            # run the following sudo command to get the start time
-            cmd = "sudo docker inspect --format='{{.State.StartedAt}}' " + \
-                container_name
+            # then perform the commands below:
+            # at the same time, record the execution time
 
-            # get the ouput as a file named "output"
-            # then read this file to get a list named "start" which contains the start time of this container
-            output = os.popen('echo %s|sudo -S %s' % (sudopassword, cmd))
-            start = output.readlines()
-            output.close()
-
-            # convert the timing format into Unix timing
-            cmd = "date -d '"+start[0]+"' +%s"
-            output = os.popen(cmd)
-            start = output.readlines()
-            output.close()
-
-            # follow the same pattern above
-            # get the finish time
-
-            cmd = "sudo docker inspect --format='{{.State.FinishedAt}}' " + \
-                container_name
-
-            output = os.popen('echo %s|sudo -S %s' % (sudopassword, cmd))
-            end = output.readlines()
-            output.close()
-
-            cmd = "date -d '"+end[0]+"' +%s"
-            output = os.popen(cmd)
-            end = output.readlines()
-            output.close()
+            start = time.perf_counter()
+            os.system(cmd)
+            end = time.perf_counter()
 
             # compute the overall runtime
-            runtime = int(end[0]) - int(start[0])
+
+            runtime = int(1000*(end-start))
 
             # store the result in "experiment_result"
 
-            experiment_result[str(alloc[j])] = str(runtime)
+            experiment_result[str(alloc[j])] = runtime
+            
 
-    return result
+    data = [0]*len(alloc)
+
+    # compute the average runtime
+
+    for ele in result:
+        for i in range(0, len(alloc)):
+            data[i] += result[ele][str(alloc[i])]
+
+    for i in range(0, len(data)):
+        data[i] /= cnt
+
+    return data
+
+
+def heatplot(Xs, Ys, Zs):
+    # copied from https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+    # using this code to draw a heatplot
+
+    Z = []
+    for i in range(0, len(Ys)):
+        Z.append(Zs[i*len(Xs): (i+1)*len(Xs)])
+
+    data = np.array(Z)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(data)
+
+    # Show all ticks and label them with the respective list entries
+
+    ax.set_xticks(np.arange(len(Xs)), labels=cpu_alloc)
+    ax.set_yticks(np.arange(len(Ys)), labels=m_alloc)
+
+    # Rotate the tick labels and set their alignment
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations
+
+    for i in range(len(Ys)):
+        for j in range(len(Xs)):
+            text = ax.text(j, i, data[i, j],
+                           ha="center", va="center", color="w")
+
+    ax.set_title("the Runtime of containers")
+    fig.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
     # configure the function
 
-    image_name = ""
-    sudopassword = ""
+    image_name = "cb90"
     cnt = 1
 
-    # first determine the allocation plan
+    # determine the allocation plan
 
     cpu_alloc = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
     m_alloc = [128, 256, 512, 768, 1024, 2048]
@@ -86,5 +129,14 @@ if __name__ == '__main__':
         for j in range(0, len(m_alloc)):
             alloc.append([cpu_alloc[i], m_alloc[j]])
 
-    # print the outcome here
-    print(experiment(alloc, image_name, cnt, sudopassword))
+    # create the containers and get their names
+
+    container_name = configure(alloc)
+
+    # run the containers to get the execution time
+
+    data = experiment(container_name, cnt, alloc)
+
+    # draw the heatplot using the data above
+
+    heatplot(cpu_alloc, m_alloc, data)

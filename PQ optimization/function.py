d8bb6d78e2aaa4e9e2afb9473d11f43077cd657d
Hongyu <435984671@qq.com>
2022-10-05 09:43:08 +0800

    Oct 4 commit

--- PQ optimization/function.py
+++ PQ optimization/function.py
@@ -1,13 +1,20 @@
 from typing import List
 from priority_queue import *
 
+
 class Function(object):
     """a monitoring of serverless functions"""
 
-    def __init__(self, cpu_alloc, mem_alloc, runtime):
+    def __init__(self, index, cpu_alloc, mem_alloc, curr_runtime):
+        self.index = index
         self.cpu_alloc = cpu_alloc
         self.mem_alloc = mem_alloc
-        self.runtime = runtime
+        self.curr_runtime = curr_runtime
+        self.last_runtime = 0
+
+    def set_runtime(self, runtime):
+        self.last_runtime = self.curr_runtime
+        self.curr_runtime = runtime
 
 
 class Workflow(Function):
@@ -22,8 +29,10 @@ class Workflow(Function):
         self.runtime = 0
 
         for func in self.workflow:
-            self.runtime += func.runtime
-        
+            self.runtime += func.curr_runtime
 
-
-
+    def init_queue(self):
+        for func in self.workflow:
+            prioirty = func.curr_runtime - func.last_runtime
+            self.cpu_pq.push((func, prioirty))
+            self.mem_pq.push((func, prioirty))
